7dcfbce492ea2dbc7beba7275f2d642f3ed37235
Mikhail Shpakov <mikhail.n.shpakov@gmail.com>
2020-03-14 02:33:49 +0300

    Исправления Heroku

--- client/src/helpers/failedToast.js
+++ client/src/helpers/failedToast.js
@@ -1,9 +1,9 @@
 import { ToastProgrammatic as Toast } from 'buefy'
 
-export default function (msg) {
+export default function (msg, type = 'failed') {
   Toast.open({
     message: msg,
     position: 'is-bottom',
-    type: 'is-danger'
+    type: type === 'success' ? 'is-success' : 'is-danger'
   })
 }
