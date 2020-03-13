bf95704a04c4cb25a71ffbf3d0d3ddd9bb5fb510
Mikhail Shpakov <mikhail.n.shpakov@gmail.com>
2020-03-14 01:12:02 +0300

    Исправления Heroku

---
+++ server/src/config/dbConfig.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'test',
+    database: 'dev',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: 'test',
+    database: 'test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    dialect: 'postgres'
+  }
+}
