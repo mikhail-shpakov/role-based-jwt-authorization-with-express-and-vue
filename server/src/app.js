64e2d03ead665e69bc6f5bb5fc41f0e9f1244f78
shpakov <mikhail.n.shpakov@gmail.com>
2020-04-07 01:19:05 +0300

    Реализован правильный механизм отдачи статичного приложения Vue при публикации на Heroku

--- server/src/app.js
+++ server/src/app.js
@@ -3,21 +3,13 @@ const path = require('path')
 const helmet = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
+const history = require('connect-history-api-fallback')
 const { errors } = require('celebrate')
 const jwtValidation = require('./middlewares/jwtValidation')
-// const jwtCheckInDb = require('./middlewares/jwtCheckInDb')
 const routes = require('./routes')
 const limiter = require('./config/rateLimitConfig')
 const app = express()
 const db = require('./db/models')
-
-if (process.env.NODE_ENV === 'production') {
-  /**
-   * При публикации на heroku,
-   * статичное приложение vue будет отдавать express
-   */
-  app.use(express.static(path.resolve(__dirname, '../../client/dist')))
-}
 
 app.set('trust proxy', 1)
 app.use(helmet())
@@ -26,9 +18,23 @@ app.use(express.json({ limit: '30mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(limiter)
+
+/**
+ * Для того, чтобы Swagger работал
+ * и при публикации на Heroku и с локальным dev сервером,
+ * размещаем роут с ним перед промежуточным обработчиком,
+ * который будет отдавать статичное приложение Vue,
+ * при публикации на Heroku
+ */
+app.use(require('./routes/swagger/'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(history())
+  app.use(express.static(path.resolve(__dirname, '../../client/dist')))
+}
+
 db.sequelize.sync()
 app.use(jwtValidation())
-// app.use(jwtCheckInDb)
 routes(app)
 app.use(errors())
 
