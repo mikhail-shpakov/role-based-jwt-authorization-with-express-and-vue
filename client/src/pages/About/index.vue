986633d4f52d2ae374ccdac48c28c16413462d4e
shpakov <mikhail.n.shpakov@gmail.com>
2020-04-07 01:35:58 +0300

    Подготовлен шаблон для создания текста на странице About

--- client/src/pages/About/index.vue
+++ client/src/pages/About/index.vue
@@ -3,11 +3,33 @@
   .about-img
   div.has-text-centered
     p About page
+    div(v-html="md")
 </template>
 
 <script>
+import marked from 'marked'
+
 export default {
-  name: 'About'
+  name: 'About',
+  data () {
+    return {
+      md_text: '# Title'
+    }
+  },
+  computed: {
+    md () {
+      marked.setOptions({
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: true,
+        pedantic: false,
+        smartLists: true,
+        smartypants: false
+      })
+      return marked(this.md_text)
+    }
+  }
 }
 </script>
 
