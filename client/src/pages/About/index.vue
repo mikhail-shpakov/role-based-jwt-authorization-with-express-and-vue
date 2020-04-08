7b2fa778a67f190a2d2d775557b4737471024daa
shpakov <mikhail.n.shpakov@gmail.com>
2020-04-08 18:42:41 +0300

    Исправлено описание проекта

--- client/src/pages/About/index.vue
+++ client/src/pages/About/index.vue
@@ -36,7 +36,7 @@
           rel="noopener"
         )  CRUD
         |  операции.
-      p.about-paragraph Имплементация JWT авторизации выполнена в полном соответствии с
+      p.about-paragraph Имплементация JWT авторизации выполнена в соответствии с
         a(
           href="https://gist.github.com/zmts/802dc9c3510d79fd40f9dc38a12bccfc"
           target="_blank"
