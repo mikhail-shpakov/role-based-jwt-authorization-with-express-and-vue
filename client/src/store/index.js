c222de74757fe0fff973b1736435ceec6d05131f
shpakov <mikhail.n.shpakov@gmail.com>
2020-04-08 18:43:13 +0300

    Реализовано шифрование localstorage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
 import user from './modules/user'
 import server from './modules/server'
+
+const ls = new SecureLS({
+  encodingType: 'aes',
+  isCompression: false
+})
 
 Vue.use(Vuex)
 
 /* eslint-disable no-new */
 const store = new Vuex.Store({
-  plugins: [createPersistedState()],
+  plugins: [createPersistedState({
+    storage: {
+      getItem: (key) => ls.get(key),
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: (key) => ls.remove(key)
+    }
+  })],
   modules: {
     user,
     server
