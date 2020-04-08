b0434ded6095bab7cc49325e4e828296b1989d23
shpakov <mikhail.n.shpakov@gmail.com>
2020-04-08 14:41:15 +0300

    Добавлено полное описание api endpoints в swagger

--- server/src/routes/swagger/index.js
+++ server/src/routes/swagger/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'))
 
 router.use('/swagger', swaggerUi.serve)
 router.get('/swagger', swaggerUi.setup(swaggerDocument))
